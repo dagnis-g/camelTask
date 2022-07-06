26232dfbb40651d3599dd90cf786435a1530377a
Aleksandr Zheltishev <azheltishevs@gmail.com>
2022-07-06 16:53:17 +0300

    aggregation strategy

---
+++ src/main/java/com/example/democamel/model/OrderTotals.java
@@ -0,0 +1,11 @@
+package com.example.democamel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class OrderTotals {
+
+    private Integer unitsSoldTotal;
+}

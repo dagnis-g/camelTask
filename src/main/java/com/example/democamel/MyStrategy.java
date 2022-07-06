be9d756f9a3fb05fa212ee8465b48aabd7b8c16f
DagnisGulbis <dagnisgulbis1@gmail.com>
2022-07-06 14:55:27 +0300

    strategy

---
+++ src/main/java/com/example/democamel/MyStrategy.java
@@ -0,0 +1,38 @@
+package com.example.democamel;
+
+import com.example.democamel.model.OrderFromCsv;
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
+
+import java.util.Map;
+
+
+public class MyStrategy implements AggregationStrategy {
+
+    @Override
+    public Exchange aggregate(Exchange previous, Exchange current) {
+
+        //TODO
+        ///What
+        //  region:     country:   columnName-value(RegionReport)
+        //    |            |           |       |
+        //    v            v           v       v
+        Map<String, Map<String, Map<String, String>>> mapRegionMapCountry;
+
+        if (previous == null) {
+
+            OrderFromCsv curr = (OrderFromCsv) current.getIn().getBody();
+            curr.setOrderId(1L);
+            System.out.println(curr.getOrderId());
+            return current;
+
+        } else {
+
+            OrderFromCsv prev = (OrderFromCsv) previous.getIn().getBody();
+            prev.setOrderId(prev.getOrderId() + 1);
+            System.out.println(prev.getOrderId());
+            return previous;
+
+        }
+    }
+}
--- src/main/java/com/example/democamel/route/MyRoutes.java
+++ src/main/java/com/example/democamel/route/MyRoutes.java
@@ -1,6 +1,6 @@
 package com.example.democamel.route;
 
-import com.example.democamel.handler.HandleOrders;
+import com.example.democamel.MyStrategy;
 import com.example.democamel.model.OrderFromCsv;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.BindyType;
@@ -18,14 +18,22 @@ public class MyRoutes extends RouteBuilder {
 //                .split(body())
 //                .filter().method(HandleOrders.class, "checkIfOrderOnline")
 //                .log("${body}");
+//
+//        from("file-watch:in")
+//                .unmarshal()
+//                .bindy(BindyType.Csv, OrderFromCsv.class)
+//                .split(body())
+//                .filter().method(HandleOrders.class, "checkIfOrderOnline")
+//                .bean(HandleOrders.class, "addOnlineOrdersToDb")
+//                .log(body().toString());
 
         from("file-watch:in")
                 .unmarshal()
+//                .csv()
                 .bindy(BindyType.Csv, OrderFromCsv.class)
-                .split(body())
-                .filter().method(HandleOrders.class, "checkIfOrderOnline")
-                .bean(HandleOrders.class, "addOnlineOrdersToDb")
-                .log(body().toString());
+                .split(body(), new MyStrategy())
+//                .log(body().toString())
+                .to("mock:out");
 
     }
 
